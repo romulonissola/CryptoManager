e744ec5968127529fd45bbfd859e2b3891253390
romulonissola <romulonissola@gmail.com>
2022-03-01 14:48:46 +0000

    fixing API offline state

--- CryptoManager.Client/src/app/app.component.ts
+++ CryptoManager.Client/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { AccountService, HTTPStatus } from './shared';
+import { AccountService, HealthService, HTTPStatus } from './shared';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     showLoader:boolean;
     constructor (private accountService: AccountService,
+                private healthService: HealthService,
                 private httpStatus: HTTPStatus,
-                private translate: TranslateService) {
+                private translate: TranslateService,
+                private router: Router) {
         this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS','pt']);
         this.translate.setDefaultLang('pt');
         const browserLang = this.translate.getBrowserLang();
@@ -21,9 +24,25 @@ export class AppComponent implements OnInit {
                 .subscribe((status: boolean) => {
                         this.showLoader = status;
                 });
+        this.router.events.subscribe(event =>{
+            this.ping(event);
+        });
     }
 
-    ngOnInit() {
-        this.accountService.populate();
+    ping(event) {
+        this.healthService.ping()
+            .subscribe(_ => {
+                this.accountService.populate();
+            },
+            error => {
+                alert("API is Offline")
+                console.log(error);
+                this.accountService.purgeAuth();
+                console.log(event.url);
+                if(event && event.url != '/login'){
+                    this.router.navigate(['login']);
+                }
+            }
+        );
     }
 }
--- CryptoManager.Client/src/app/shared/services/health.service.ts
+++ CryptoManager.Client/src/app/shared/services/health.service.ts
@@ -2,10 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { Health } from '../models/health.model';
-
-import { ApiService } from './api.service';
-
 @Injectable()
 export class HealthService {
 
@@ -22,6 +18,13 @@ export class HealthService {
   getAll(): Observable<any>{
     return this.http.get(
       `${environment.api.baseUrl}${this.serviceURL}`,
+      {headers: this.GetHttpHeaders()}
+    ).pipe();
+  }
+
+  ping(): Observable<any>{
+    return this.http.get(
+      `${environment.api.baseUrl}${this.serviceURL}/ping`,
       {headers: this.GetHttpHeaders()}
     ).pipe();
   }
